40f7ede775e39cc6987d206602fd523fb8c9353e
Saeed JS <saeed.ahmad@diyainteractive.com>
2018-09-07 09:19:14 +0500

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hi people</h1>
+    <p>Welcome to Saeed's new Gatsby site.</p>
+    <p>Let us code to make world a better place!</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
